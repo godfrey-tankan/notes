68d74e72c8c7ed6a3d411be09fecd8ebf92f815f
godfre-tankan <godfreytantswakandeya@gmail.com>
2024-02-21 17:46:39 +0200

    Remove commented out code in views.py

--- Django-toDO-App/api/views.py
+++ Django-toDO-App/api/views.py
@@ -78,15 +78,3 @@ def getNote(request, pk):
 #     )
 #     serializer = NoteSerializer(note, many=False)
 #     return Response(serializer.data)
-
-
-# @api_view(['PUT'])
-# def updateNote(request, pk):
-#     data = request.data
-#     note = Note.objects.get(id=pk)
-#     serializer = NoteSerializer(instance=note, data=data)
-
-#     if serializer.is_valid():
-#         serializer.save()
-
-#     return Response(serializer.data) 
