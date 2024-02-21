116cf9e6e644be86d14355214a7265c791d79a02
godfre-tankan <godfreytantswakandeya@gmail.com>
2024-02-21 17:44:49 +0200

    Refactor getNotesList function

--- Django-toDO-App/api/utils.py
+++ Django-toDO-App/api/utils.py
@@ -3,11 +3,7 @@ from .models import Note
 from .serializers import NoteSerializer
 
 
-def getNotesList(request):
-    notes = Note.objects.all().order_by('-updated')
-    serializer = NoteSerializer(notes, many=True)
-    return Response(serializer.data)
-
+#
 
 def getNoteDetail(request, pk):
     notes = Note.objects.get(id=pk)
