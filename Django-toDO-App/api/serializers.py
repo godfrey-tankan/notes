801aebd22d8db9a3588712918281419d0c5639f5
godfre-tankan <godfreytantswakandeya@gmail.com>
2024-02-21 17:43:15 +0200

    Remove unnecessary code and update database

--- Django-toDO-App/api/serializers.py
+++ Django-toDO-App/api/serializers.py
@@ -2,7 +2,3 @@ from rest_framework.serializers import ModelSerializer
 from .models import Note
 
 
-class NoteSerializer(ModelSerializer):
-    class Meta:
-        model = Note
-        fields = '__all__'
